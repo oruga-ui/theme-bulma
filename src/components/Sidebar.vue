71a44843c0850b1690cdbf2f8c75fd06e15c2558
Pablo Sirera <psirera4@gmail.com>
2021-10-13 21:28:06 +0200

    feat: use buefy style on sidebar component

---
+++ src/components/Sidebar.vue
@@ -0,0 +1,65 @@
+<template>
+    <section>
+        <o-sidebar
+            :fullheight="isFullheight"
+            :fullwidth="isFullwidth"
+            :overlay="isOverlay"
+            :right="isRight"
+            :open.sync="isOpen"
+            @close="toggleSidebar()"
+            >
+            <o-button
+              v-if="isFullwidth"
+              icon-left="times"
+              label="Close"
+              @click="toggleSidebar()"
+            />
+            <img
+                width="128"
+                src="https://avatars2.githubusercontent.com/u/66300512?s=200&v=4"
+                alt="Lightweight UI components for Vue.js"
+            />
+            <h3>Example</h3>
+           
+        </o-sidebar>
+        <div class="block">
+            <o-field grouped group-multiline>
+                <o-switch v-model="isOverlay">Overlay</o-switch>
+                <o-switch v-model="isFullheight">Fullheight</o-switch>
+                <o-switch v-model="isFullwidth">Fullwidth</o-switch>
+                <o-switch v-model="isRight">Right</o-switch>
+            </o-field>
+        </div>
+
+        <br>
+
+        <o-button @click="toggleSidebar()">Show</o-button>
+    </section>
+</template>
+
+<script>
+import { ref } from 'vue'
+
+export default {
+  setup() {
+    const isOpen = ref(false)
+    const isOverlay = ref(true)
+    const isFullheight = ref(true)
+    const isFullwidth = ref(false)
+    const isRight = ref(false)
+
+    const toggleSidebar = () => {
+      isOpen.value = !isOpen.value
+    }
+
+    return {
+      toggleSidebar,
+      isOpen,
+      isOverlay,
+      isFullheight,
+      isFullwidth,
+      isRight
+    }
+  }
+}
+</script>
