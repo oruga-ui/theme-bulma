3d376f7a903edb65ee60438ba1b191e04559d22c
adam <adam@harpur.io>
2021-10-20 21:24:26 +0100

    chore: initial rollup config

---
+++ rollup.config.js
@@ -0,0 +1,12 @@
+import copy from "rollup-plugin-copy"
+import scss from "rollup-plugin-scss"
+
+const entries = {
+  index: "src/plugins/oruga.ts",
+  scss: "src/assets/scss/plugin.scss",
+}
+
+export default function() {
+  const config = []
+  return config
+}
