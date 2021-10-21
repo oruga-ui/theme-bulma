50e5ec675b3c1f7a328ae9956427d831699355e2
adam <adam@harpur.io>
2021-10-21 13:46:40 +0100

    chore: update rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -1,12 +1,94 @@
+import cjs from "@rollup/plugin-commonjs"
 import copy from "rollup-plugin-copy"
-import scss from "rollup-plugin-scss"
+import sass from "rollup-plugin-sass"
+import typescript from "@rollup/plugin-typescript"
+
+import autoprefixer from "autoprefixer"
+import fs from "fs"
+import path from "path"
+import postcss from "postcss"
+
+import pkg from "./package.json"
 
 const entries = {
   index: "src/plugins/oruga.ts",
-  scss: "src/assets/scss/plugin.scss",
+  scss: "src/assets/scss",
+}
+
+const exits = {
+  scss: "dist",
+  css: "dist/oruga-bulma.css",
+  esm: `dist/${pkg.module}`,
+  umd: `dist/${pkg.main}`,
+}
+
+const commonSassPluginOptions = {
+  options: {
+    includePaths: ["node_modules"],
+  },
+}
+
+const typescriptPluginOptions = {
+  sourceMap: false,
+}
+
+function createDirectoryIfDoesNotExist(filePath) {
+  const directoryName = path.dirname(filePath)
+  if (fs.existsSync(directoryName)) {
+    return true
+  }
+  createDirectoryIfDoesNotExist(directoryName)
+  fs.mkdirSync(directoryName)
 }
 
 export default function() {
-  const config = []
+  const config = [
+    {
+      input: entries.index,
+      output: {
+        format: "umd",
+        name: `${pkg.name.replace(/ /g, "")}`,
+        file: `${exits.umd}`,
+      },
+      plugins: [
+        copy({
+          targets: [
+            { src: `${entries.scss}`, dest: `${exits.scss}` },
+            { src: "package.json", dest: "dist" },
+          ],
+        }),
+        cjs(),
+        sass({
+          processor: async (css) => {
+            const { css: processedCss } = await postcss([autoprefixer]).process(
+              css,
+              {
+                from: undefined,
+              }
+            )
+            return processedCss
+          },
+          output(styles) {
+            createDirectoryIfDoesNotExist(exits.css)
+            fs.writeFileSync(exits.css, styles)
+          },
+          ...commonSassPluginOptions,
+        }),
+        typescript(typescriptPluginOptions),
+      ],
+    },
+    {
+      input: entries.index,
+      output: {
+        format: "esm",
+        file: `${exits.esm}`,
+      },
+      plugins: [
+        cjs(),
+        sass(commonSassPluginOptions),
+        typescript(typescriptPluginOptions),
+      ],
+    },
+  ]
   return config
 }
