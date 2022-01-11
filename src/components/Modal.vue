6c03268b3277341b1d1652607792de280e0b3b95
Florian Busch <busch@cs.rwth-aachen.de>
2022-01-11 13:13:11 +0100

    Add demo for programmatic models

--- src/components/Modal.vue
+++ src/components/Modal.vue
@@ -1,5 +1,6 @@
 <template>
   <section>
+    <h2>Base</h2>
     <div class="buttons">
       <o-button
         size="medium"
@@ -17,10 +18,17 @@
       </o-button>
     </div>
 
+    <h2>Programmatically</h2>
+    <div class="buttons">
+      <o-button size="medium" variant="primary" @click="cardModal()">
+        Open modal (Component)
+      </o-button>
+    </div>
+
     <o-modal v-model:active="isImageModalActive" content-class="modal-content">
       <p style="text-align: center">
         <img
-          style="background-color: white;"
+          style="background-color: white"
           src="https://avatars2.githubusercontent.com/u/66300512?s=200&v=4"
         />
       </p>
@@ -28,60 +36,80 @@
 
     <o-modal v-model:active="isCardModalActive" :width="640" scroll="clip">
       <form action="">
-                <div class="modal-card" style="width: auto">
-                    <header class="modal-card-head">
-                        <p class="modal-card-title">Login</p>
-                        <o-icon
-                            clickable
-                            native-type="button"
-                            icon="times"
-                            @click="isCardModalActive = false"/>
-                    </header>
-                    <section class="modal-card-body">
-                        <o-field label="Email">
-                            <o-input
-                                type="email"
-                                :value="email"
-                                placeholder="Your email"
-                                required>
-                            </o-input>
-                        </o-field>
+        <div class="modal-card" style="width: auto">
+          <header class="modal-card-head">
+            <p class="modal-card-title">Login</p>
+            <o-icon
+              clickable
+              native-type="button"
+              icon="times"
+              @click="isCardModalActive = false"
+            />
+          </header>
+          <section class="modal-card-body">
+            <o-field label="Email">
+              <o-input
+                type="email"
+                v-model="email"
+                placeholder="Your email"
+                required
+              >
+              </o-input>
+            </o-field>
 
-                        <o-field label="Password">
-                            <o-input
-                                type="password"
-                                :value="password"
-                                password-reveal
-                                placeholder="Your password"
-                                required>
-                            </o-input>
-                        </o-field>
+            <o-field label="Password">
+              <o-input
+                type="password"
+                v-model="password"
+                password-reveal
+                placeholder="Your password"
+                required
+              >
+              </o-input>
+            </o-field>
 
-                        <o-field>
-                            <o-checkbox>Remember me</o-checkbox>
-                        </o-field>
-                    </section>
-                    <footer class="modal-card-foot">
-                        <o-button type="button" @click="isCardModalActive = false">Close</o-button>
-                        <o-button variant="primary">Login</o-button>
-                    </footer>
-                </div>
-            </form>
+            <o-field>
+              <o-checkbox>Remember me</o-checkbox>
+            </o-field>
+          </section>
+          <footer class="modal-card-foot">
+            <o-button type="button" @click="isCardModalActive = false"
+              >Close</o-button
+            >
+            <o-button variant="primary">Login</o-button>
+          </footer>
+        </div>
+      </form>
     </o-modal>
   </section>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "@vue/runtime-core";
+import ModalDemoFormVue from "./ModalDemoForm.vue";
+import { ModalProgrammatic as Modal } from "@oruga-ui/oruga-next";
 
 export default defineComponent({
   data() {
     return {
       isImageModalActive: false,
       isCardModalActive: false,
-      email:'',
-      password:''
+      email: "",
+      password: "",
     };
+  },
+
+  methods: {
+    cardModal() {
+      Modal.open({
+        component: ModalDemoFormVue,
+        trapFocus: true,
+        props: {
+          email: this.email,
+          password: this.password,
+        },
+      });
+    },
   },
 });
 </script>
