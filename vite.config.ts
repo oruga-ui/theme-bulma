6ac6c0c4fa239f44ffefd23d57bb507d8b3e420f
mlmo <marcel.moravek@yahoo.de>
2024-07-18 15:36:47 +0200

    chore(oruga): update oruga

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,14 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 
+import { fileURLToPath } from "url";
+
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [vue()],
+    resolve: {
+        alias: {
+            "@": fileURLToPath(new URL("./src", import.meta.url)),
+        },
+    },
 });
