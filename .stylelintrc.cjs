7b4e52c5c805395359879c20e6ac0e3eaebcfa8a
mmoravek <marcel.moravek@init.de>
2024-07-30 13:47:59 +0200

    build(stylelint): add stylelint

---
+++ .stylelintrc.cjs
@@ -0,0 +1,27 @@
+module.exports = {
+  extends: [
+    "stylelint-config-recommended",
+    "stylelint-config-recommended-scss",
+    "stylelint-prettier/recommended",
+  ],
+  plugins: [
+    "stylelint-prettier", 
+    "stylelint-scss",
+  ],
+  rules: {
+    "scss/at-extend-no-missing-placeholder": null,
+    "no-duplicate-selectors": [true, { "severity": "warning" }],
+    "no-descending-specificity": [true, { "severity": "warning" }],
+    "rule-empty-line-before": ["always", { 
+      except: ["first-nested"],
+      ignore: ["after-comment"],
+    }],
+    "at-rule-empty-line-before": ["always", { 
+      except: ["first-nested", "after-same-name"],
+      ignore: ["after-comment", "blockless-after-blockless"],
+      ignoreAtRules: ["if", "else"]
+    }],
+    "prettier/prettier": [true, { endOfLine: "auto" },
+    ],
+  },
+};
