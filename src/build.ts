5cc8e96c7f5621cc9d74850f9b548c917ec639bc
mlmoravek <marcel.moravek@yahoo.de>
2025-03-10 09:53:07 +0100

    chore: lint code

--- src/build.ts
+++ src/build.ts
@@ -1,4 +1,4 @@
-// This file is intended to the be the main entrypoint for the theme's rollup build. Separating this out from theme.ts allows us to include the styles in the build without creating duplicate styles in the test app
+// This file is intended to the be the main entrypoint for the theme's rollup build.
 
 import "./assets/scss/bulma-build.scss";
 export * from "./plugins/theme.ts";
