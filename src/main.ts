f6b0e7c8f8e0698d14ee1bd78edca808b5630b68
Ben Webb <ben.webb@whiplash.com>
2025-05-27 14:00:33 -0400

    fix(build) : host fix

--- src/main.ts
+++ src/main.ts
@@ -9,9 +9,10 @@ import Examples from "@oruga-ui/examples";
 // add examples styles
 import "@oruga-ui/examples/dist/examples.css";
 // import main bundle, either separated or combined
-// this import's primary purpose is to import styles for use in the main css bundle (dist/bulma.css). it can also be used for testing the separate build locally for testing purposes. do to this just swap the main-combined import for main-separated. PLEASE NOTE you MUST switch this back before your merge, otherwise the docs will break.
-import "./main-combined.scss";
-//import "./main-separated.scss";
+// these imports are for testing different ways of including theme/bulma styles
+// use one or the other. do not use both.
+//import "./main-combined.scss";
+import "./main-separated.scss";
 
 // add theme config
 import { bulmaConfig } from "./plugins/theme";
